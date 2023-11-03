81c1f9bd4ca8d1e391959c3ca9d59953323a1f89
ybenel <root@ybenel.cf>
2023-11-03 23:14:42 +0100

    Adding sday analyses

--- utils/analyze.go
+++ utils/analyze.go
@@ -1,11 +1,58 @@
 package utils
 
-// Analyze a year worth of numbers.
-// Analyzing each day, each time of day, each month.
+import (
+	"fmt"
+)
 
+var (
+	Days = []string{"Monday", "Tuesday", "Thursday", "Wednesday", "Friday", "Saturday", "Sunday"}
+)
 
+type DataHold struct {
+	Nums  string
+	DayT  string
+	Month int
+	Year  int
+}
 
+const (
+	Year    = 2023
+	NumIter = 11
+)
 
-func (m *DB) AnalyzeDay([]uint64) {
+// Analyzes Everyday of all months of a year
+func (m *DB) AnalyzeSDay() {
+	m.L.Logger.Log.Info("Analyzing All Stored 10 months")
+	datahold := make([]DataHold, 0)
+	for n := 1; n < NumIter; n++ {
+		for _, day := range Days {
+			for i := 1; i < 4; i++ {
+				m.L.Logger.Log.Debugf("Collecting Month %d, day %s, Entry %d", n, day, i)
 
+				nums, err := m.NumGetSday(Year, n, day, Times[i])
+				if err != nil {
+					m.L.Logger.Log.Error(err)
+					continue
+				}
+
+				numbers := make([]uint64, len(nums))
+				copy(numbers, nums)
+
+				Unlisted := m.L.FindDupNums(numbers, true)
+				rangedNums, err := FromJson([]byte(Unlisted), true)
+				if err != nil {
+					m.L.Logger.Log.Error(err)
+					continue
+				}
+
+				key := fmt.Sprintf("%s-%s", day, Times[i])
+				jsonNums := m.L.toJson(rangedNums)
+				datahold = append(datahold, DataHold{Nums: jsonNums, DayT: key, Month: n, Year: Year})
+			}
+		}
+	}
+	err := m.InsertTo("AnalyzedD", datahold)
+	if err != nil {
+		m.L.Logger.Log.Error(err)
+	}
 }
