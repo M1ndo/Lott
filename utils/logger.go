941c62c2eebb8018d138fb9d5daf1e6f92ee8efb
ybenel <root@ybenel.cf>
2023-11-05 23:50:07 +0100

    Exporting Debug

--- utils/logger.go
+++ utils/logger.go
@@ -9,13 +9,15 @@ type Logger struct {
 	*loggdb.Logger
 }
 
+var Debug = loggdb.Debug
+
 func (util *Utils) SetLogger() {
 	CustomOpt := &loggdb.CustomOpt{
 		Prefix:          "LoTT 👾 ",
 		ReportTimestamp: true,
 	}
-	util.Logger = Logger{Logger: &loggdb.Logger{}}
-	util.Logger.LogOptions = CustomOpt
-	util.Logger.NewLogger()
-	util.Logger.Log.SetColorProfile(termenv.TrueColor)
-}
+		util.Logger = Logger{Logger: &loggdb.Logger{}}
+		util.Logger.LogOptions = CustomOpt
+		util.Logger.NewLogger()
+		util.Logger.Log.SetColorProfile(termenv.TrueColor)
+	}
